c5258d1cf6fb514bdd833ce4ebb720fb6be90fed
“Dhairya <121856146+dhairya-p@users.noreply.github.comgit config --global user.email 121856146+dhairya-p@users.noreply.github.com>
2025-03-22 16:07:14 +0800

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import SecurityDNAVisualizer from './components/SecurityDNAVisualizer';
 import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900">
+    <>
       <SecurityDNAVisualizer />
-    </div>
+    </>
   );
 }
 
